9b403e6f2f11babed86266e5bdff630fb5371294
Srinivasulu <srinuvasulucsv@gmail.com>
2026-08-19 23:33:00 +0530

    updating config files

--- 003-payment-service/src/main/java/com/payment/Application.java
+++ 003-payment-service/src/main/java/com/payment/Application.java
@@ -1,11 +1,9 @@
-package com.config;
+package com.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer
 public class Application {
 
 	public static void main(String[] args) {
